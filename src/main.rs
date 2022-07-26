810ee0540863743e413603527c732a366d43d80c
jfont <jdafontenot@gmail.com>
2022-07-26 10:20:17 -0400

    Individual packet processing and basic object/movement loop

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::collections::HashMap;
+use std::collections::hash_map::ValuesMut;
 
 use bytes::*;
 use tokio::net::tcp::{OwnedWriteHalf, OwnedReadHalf};
@@ -12,8 +13,13 @@ mod packets;
 
 use crate::packets::*;
 
+extern crate nalgebra as na;
+
 type Error = Box<dyn std::error::Error>;
 type Result<T> = core::result::Result<T, Error>;
+
+const DELTA: f32 = 1.0/60.0;
+const SPEED: f32 = 200.0;
 
 #[derive(Debug)]
 struct PacketFrame {
@@ -68,24 +74,42 @@ async fn handle_client(mut socket: OwnedReadHalf, id: u32, tx: Sender<PacketFram
     }
 }
 
+struct Position {
+    x: f32,
+    y: f32
+}
+
 struct Player {
     username: String,
     connection_id: u32,
-    object_id: u32
+    object_id: u32,
+    position: Position
 }
 
 struct GameState {
     connections: HashMap<u32, Connection>,
-    players: Vec<Player>
+    players: HashMap<u32, Player>
 }
 
-async fn run_server(mut connection_rx: Receiver<Connection>) {
+async fn broadcast_packet(connections: &mut HashMap<u32, Connection>, packet: Packet, ignore_id: u32) -> Result<()> {
+    for conn in connections.values_mut() {
+        if conn.id == ignore_id {
+            continue;
+        }
+
+        packet.send_packet(&mut conn.tx).await?;
+    }
+    return Ok(())
+}
+
+async fn run_server(mut connection_rx: Receiver<Connection>) -> ! {
     let mut object_id = 0;
 
     let mut state = GameState {
         connections: HashMap::new(),
-        players: vec![]
+        players: HashMap::new()
     };
+
 
     loop {
         // Check for connections
@@ -115,7 +139,6 @@ async fn run_server(mut connection_rx: Receiver<Connection>) {
             loop {
                 match conn.rx.try_recv() {
                     Ok(packet_frame) => {
-                        println!("Got a packet from {}", &conn.id);
                         packets.push(packet_frame);
                     },
                     Err(error::TryRecvError::Empty) => {
@@ -129,8 +152,19 @@ async fn run_server(mut connection_rx: Receiver<Connection>) {
                 }
             }
         }
-        for id in drop_conns {
+
+        for id in drop_conns.iter() {
             state.connections.remove(&id).unwrap();
+        }
+
+        for id in drop_conns.iter() {
+            if let Some(player) = state.players.get(id) {
+                let remove_player = Packet::ObjectRemovedEvent(ObjectRemovedEventData {
+                    object_id: player.object_id
+                });
+                let _ = broadcast_packet(&mut state.connections, remove_player, *id).await;
+            }
+            let _ = state.players.remove(id);
         }
 
         for packet_frame in packets {
@@ -148,16 +182,86 @@ async fn run_server(mut connection_rx: Receiver<Connection>) {
 
                     let resp = response.send_packet(&mut conn.tx).await;
                     resp.expect("Failed to write packet");
-                    println!("Wrote a packet to {}", &conn.id);
 
-                    state.players.push(Player {
+                    for (_connection_id, player) in state.players.iter() {
+                        let new_player = Packet::NewObjectEvent(NewObjectEventData {
+                            name: player.username.clone(),
+                            object_id: player.object_id,
+                            x: player.position.x,
+                            y: player.position.y
+                        });
+                        let _ = new_player.send_packet(&mut conn.tx).await;
+                    }
+
+                    state.players.insert(conn.id, Player {
                         connection_id: conn.id,
                         object_id: id,
-                        username: name
+                        username: name.clone(),
+                        position: Position {
+                            x: 0.0,
+                            y: 0.0
+                        }
                     });
+
+                    let new_player = Packet::NewObjectEvent(NewObjectEventData {
+                        name: name.clone(),
+                        object_id: id,
+                        x: 0.0,
+                        y: 0.0
+                    });
+                    let _ = broadcast_packet(&mut state.connections, new_player, packet_frame.connection_id).await;
                 },
                 Packet::MoveRequest(data) => {
+                    let a_x: f32 = {
+                        if data.right { 1.0 }
+                        else if data.left { -1.0 }
+                        else { 0.0 }
+                    };
 
+                    let a_y: f32 = {
+                        if data.down { 1.0 }
+                        else if data.up { -1.0 }
+                        else { 0.0 }
+                    };
+
+                    let conn = state.connections.get_mut(&packet_frame.connection_id).unwrap();
+
+                    let player = state.players.get_mut(&packet_frame.connection_id).expect("Could not find player by conn id");
+
+                    let stored_pos = &mut player.position;
+
+                    if a_x == 0.0 && a_y == 0.0 {
+                        let resp = Packet::MoveResponse(MoveResponseData {
+                            confirm: true,
+                            new_x: stored_pos.x,
+                            new_y: stored_pos.y
+                        });
+
+                        let _ = resp.send_packet(&mut conn.tx).await;
+                        continue;
+                    }
+                    let pos = na::Vector2::new(stored_pos.x, stored_pos.y);
+                    let accel = na::Vector2::new(a_x, a_y).normalize();
+                    let new_pos = pos + SPEED * DELTA * accel;
+                    let resp = Packet::MoveResponse(MoveResponseData {
+                        confirm: true,
+                        new_x: new_pos.x,
+                        new_y: new_pos.y
+                    });
+
+                    stored_pos.x = new_pos.x;
+                    stored_pos.y = new_pos.y;
+
+                    let _ = resp.send_packet(&mut conn.tx).await;
+
+                    let player_moved = Packet::ObjectMovedEvent(ObjectMovedEventData {
+                        object_id: player.object_id,
+                        x: new_pos.x,
+                        y: new_pos.y
+                    });
+
+                    let _ = resp.send_packet(&mut conn.tx).await;
+                    broadcast_packet(&mut state.connections, player_moved, packet_frame.connection_id).await;
                 },
                 _ => {
                     eprintln!("Unexpected packet recv'd");
@@ -182,6 +286,7 @@ async fn main() -> Result<()> {
     let mut i = 0;
     loop {
         let (socket, _sock_addr) = listener.accept().await?;
+        socket.set_nodelay(true)?;
         let (sock_read, sock_write) = socket.into_split();
         i += 1;
 
--- src/packets.rs
+++ src/packets.rs
@@ -1,7 +1,7 @@
 use bytes::{BytesMut, BufMut, Bytes, Buf};
 use tokio::{net::tcp::OwnedWriteHalf, io::AsyncWriteExt};
 
-use crate::serialize::read_string;
+use crate::serialize::{read_string, write_string};
 
 #[non_exhaustive]
 pub struct PacketType;
@@ -11,6 +11,9 @@ impl PacketType {
 	pub const InitUserResponse: u32 = 1;
 	pub const MoveRequest: u32 = 2;
 	pub const MoveResponse: u32 = 3;
+    pub const NewObjectEvent: u32 = 4;
+    pub const ObjectMovedEvent: u32 = 5;
+    pub const ObjectRemovedEvent: u32 = 6;
 }
 
 #[derive(Debug)]
@@ -18,7 +21,10 @@ pub enum Packet {
     InitUserRequest(InitUserRequestData),
     InitUserResponse(InitUserResponseData),
     MoveRequest(MoveRequestData),
-    MoveResponse(MoveResponseData)
+    MoveResponse(MoveResponseData),
+    NewObjectEvent(NewObjectEventData),
+    ObjectMovedEvent(ObjectMovedEventData),
+    ObjectRemovedEvent(ObjectRemovedEventData)
 }
 
 #[derive(Debug)]
@@ -33,8 +39,10 @@ pub struct InitUserResponseData {
 
 #[derive(Debug)]
 pub struct MoveRequestData {
-    pub x: f32,
-    pub y: f32
+    pub up: bool,
+    pub down: bool,
+    pub left: bool,
+    pub right: bool
 }
 
 #[derive(Debug)]
@@ -44,23 +52,63 @@ pub struct MoveResponseData {
     pub new_y: f32
 }
 
+#[derive(Debug)]
+pub struct NewObjectEventData {
+    pub object_id: u32,
+    pub x: f32,
+    pub y: f32,
+    pub name: String
+}
+
+#[derive(Debug)]
+pub struct ObjectMovedEventData {
+    pub object_id: u32,
+    pub x: f32,
+    pub y: f32
+}
+
+#[derive(Debug)]
+pub struct ObjectRemovedEventData {
+    pub object_id: u32
+}
+
 impl Packet {
-    pub async fn send_packet(self, tx: &mut OwnedWriteHalf) -> std::result::Result<(), std::io::Error>{
+    pub async fn send_packet(&self, tx: &mut OwnedWriteHalf) -> std::result::Result<(), std::io::Error>{
         let mut buf = BytesMut::new();
         match self {
             Packet::InitUserResponse(data) => {
-                buf.put_u32_le(1);
+                buf.put_u32_le(PacketType::InitUserResponse);
                 buf.put_u32_le(data.object_id);
             },
             Packet::MoveResponse(data) => {
+                buf.put_u32_le(PacketType::MoveResponse);
                 buf.put_u8(if data.confirm { 1 } else { 0 });
                 buf.put_f32_le(data.new_x);
                 buf.put_f32_le(data.new_y);
+            },
+            Packet::NewObjectEvent(data) => {
+                buf.put_u32_le(PacketType::NewObjectEvent);
+                buf.put_u32_le(data.object_id);
+                buf.put_f32_le(data.x);
+                buf.put_f32_le(data.y);
+                write_string(&mut buf, &data.name);
+            },
+            Packet::ObjectMovedEvent(data) => {
+                buf.put_u32_le(PacketType::ObjectMovedEvent);
+                buf.put_u32_le(data.object_id);
+                buf.put_f32_le(data.x);
+                buf.put_f32_le(data.y);
+            },
+            Packet::ObjectRemovedEvent(data) => {
+                buf.put_u32_le(PacketType::ObjectRemovedEvent);
+                buf.put_u32_le(data.object_id);
             }
             _ => {}
         }
 
-        return tx.write_all_buf(&mut buf).await;
+        let r = tx.write_all_buf(&mut buf).await;
+        let r2 = tx.flush().await;
+        return r2;
     }
 
     pub fn read_packet(mut buf: Bytes) -> Option<Packet> {
@@ -75,8 +123,10 @@ impl Packet {
             },
             PacketType::MoveRequest => {
                 Some(Packet::MoveRequest(MoveRequestData {
-                    x: buf.get_f32_le(),
-                    y: buf.get_f32_le()
+                    up: buf.get_u8() == 1,
+                    down: buf.get_u8() == 1,
+                    left: buf.get_u8() == 1,
+                    right: buf.get_u8() == 1
                 }))
             }
             _ => {
